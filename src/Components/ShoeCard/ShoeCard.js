a034c472541066836b3d5dcd4b10a8b28465d69f
Markus Rossio <argitavis@gmail.com>
2022-02-11 01:07:32 -0600

    Add all shoe data to shoe card

--- src/Components/ShoeCard/ShoeCard.js
+++ src/Components/ShoeCard/ShoeCard.js
@@ -3,7 +3,12 @@ import React from "react";
 export default function ShoeCard({ shoeData }) {
   return (
     <li id={shoeData.id}>
+      <img src={shoeData.url} alt={shoeData.description}/>
       <h2>{shoeData.description}</h2>
+      <p>{shoeData.brand}</p>
+      <p>{shoeData.size}</p>
+      <p>{shoeData.side}</p>
+      <p>{shoeData.style}</p>
     </li>
   )
 }
