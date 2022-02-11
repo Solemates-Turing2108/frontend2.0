36cb6e8de20bdca8aad15c3df6800dfe9ae1beb6
Markus Rossio <argitavis@gmail.com>
2022-02-11 03:55:33 -0600

    Add jpg extenxtion to valid data 0, Add function to shoe card that works around cors error for image loading

--- src/Components/ShoeCard/ShoeCard.js
+++ src/Components/ShoeCard/ShoeCard.js
@@ -2,6 +2,18 @@ import React from "react";
 import './ShoeCard.css';
 
 export default function ShoeCard({ shoeData }) {
+  const allowCORSImg = () => {
+    const url = shoeData.photo_url;
+    const image = new Image();
+    image.src = url;
+
+    const corsImage = new Image();
+    corsImage.crossOrigin = "Anonymous";
+    corsImage.src = url;
+    console.log(corsImage)
+    return corsImage.src;
+  }
+
   return (
     <li className="product-card_container" id={shoeData.id}>
       <img className="product-card_img" src={shoeData.photo_url} alt={shoeData.description}/>
