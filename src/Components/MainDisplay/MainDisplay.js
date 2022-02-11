a6c3fd00a13ba1beebaa36cd39f016b23db3c487
Markus Rossio <argitavis@gmail.com>
2022-02-10 23:56:31 -0600

    Move test data directory into src due to error, Import use effect into main display js, Create function that uses getallshoes and sets the data into main display state on componenet mount

--- src/Components/MainDisplay/MainDisplay.js
+++ src/Components/MainDisplay/MainDisplay.js
@@ -1,7 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { getAllShoes } from "../../Services/api/apiCalls";
 
 export default function MainDisplay() {
   const [shoeData, setShoeData] = useState([]);
+  
+  useEffect(() => {
+    updateShoeData();
+  },[]);
+
+  const updateShoeData = async () => {
+    const { shoes } = await getAllShoes();
+    setShoeData(shoes);
+  }
 
   return (
     <h1>Dog Blog</h1>
--- src/Services/api/apiCalls.js
+++ src/Services/api/apiCalls.js
@@ -1,4 +1,4 @@
-import { returnValidShoes } from "../TestData/validData";
+import { returnValidShoes } from "../../TestData/validData";
 
 const getAllShoes = async () => {
   const allShoes = await returnValidShoes();
