6d20d5b254d7f7eb2cfb5441d1e824a9cf843cb1
Markus Rossio <argitavis@gmail.com>
2022-02-11 11:57:53 -0600

    Add file extention to photo url on shoe 0

---
+++ src/TestData/validData.js
@@ -0,0 +1,42 @@
+const shoes = {
+  "shoes":[
+    {
+      "brand": "nike",
+      "description": "one cool shoe",
+      "id": 1,
+      "photo_url": "https://imgur.com/67Zsobn.jpg",
+      "side": "left",
+      "size": 10,
+      "style": "sneaker",
+      "user_id": 3
+      },
+      {
+      "brand": "redwing",
+      "description": "there's a boot in my snake",
+      "id": 2,
+      "photo_url": "https://i.imgur.com/8PHxguM.jpg",
+      "side": "right",
+      "size": 9,
+      "style": "boot",
+      "user_id": 2
+      },
+      {
+      "brand": "Nordstrom",
+      "description": "super slick shoe",
+      "id": 3,
+      "photo_url": "https://i.imgur.com/nAQO1Tk.jpg",
+      "side": "left",
+      "size": 12,
+      "style": "dress",
+      "user_id": 1
+      }
+  ]
+}
+
+const returnValidShoes = () => {
+  return new Promise ((resolve, reject) => {
+    setTimeout(() => resolve(shoes), 1000);
+  });
+};
+
+export { returnValidShoes };
