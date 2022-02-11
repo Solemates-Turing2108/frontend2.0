f8dfbee0b960bfb05000ddbba03ff570d777eaea
Markus Rossio <argitavis@gmail.com>
2022-02-11 03:38:10 -0600

    Change img src in shoe card to photo_url

--- src/Components/ShoeCard/ShoeCard.js
+++ src/Components/ShoeCard/ShoeCard.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function ShoeCard({ shoeData }) {
   return (
     <li id={shoeData.id}>
-      <img src={shoeData.url} alt={shoeData.description}/>
+      <img src={shoeData.photo_url} alt={shoeData.description}/>
       <h2>{shoeData.description}</h2>
       <p>{shoeData.brand}</p>
       <p>{shoeData.size}</p>
