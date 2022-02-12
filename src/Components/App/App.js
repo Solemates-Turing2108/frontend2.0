d2f01d02a7e0927a885d00bdbff2df9447f5c74e
Markus Rossio <argitavis@gmail.com>
2022-02-12 01:32:11 -0600

    Add listing form component add path for listing form

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,14 +4,14 @@ import './App.css';
 import MainDisplay from './MainDisplay/MainDisplay';
 import DashboardPage from './DashboardPage';
 import DetailsPage from './DetailsPage';
-
+import ListingForm from '../ListingForm/ListingForm';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainDisplay />} />
+      <Route path="add-listing" element={<ListingForm />}/>
     </Routes>
-
   );
 }
 
---
+++ src/Components/ListingForm/ListingForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function ListingForm() {
+  return (
+    <h1>Listing Form</h1>
+  )
+}
