67d192be2dc9489ed3527051bc4a9c0a32227c51
Markus Rossio <argitavis@gmail.com>
2022-02-12 01:23:08 -0600

    Add routes to app, Add route for home rendering main display componenet

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import MainDisplay from './MainDisplay/MainDisplay';
 import DashboardPage from './DashboardPage';
@@ -7,10 +8,11 @@ import DetailsPage from './DetailsPage';
 
 function App() {
   return (
-    <>
-      <MainDisplay />
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<MainDisplay />} />
+    </Routes>
 
+  );
 }
+
 export default App;
