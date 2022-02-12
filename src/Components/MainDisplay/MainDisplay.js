0ded5cf42ee2ed1892608491f20963a57c952b18
Markus Rossio <argitavis@gmail.com>
2022-02-12 00:01:34 -0600

    Remove duplicate function in main display

--- src/Components/MainDisplay/MainDisplay.js
+++ src/Components/MainDisplay/MainDisplay.js
@@ -19,11 +19,6 @@ export default function MainDisplay() {
     updateShoeData();
   },[]);
 
-  const updateShoeData = async () => {
-    const { shoes } = await getAllShoes();
-    setShoeData(shoes);
-  }
-
   const createShoeCards = () => {
     if (shoeData.length) {
       const shoeCards = shoeData.map(data => {
