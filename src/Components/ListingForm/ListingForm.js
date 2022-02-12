fc98bf7f1cf97c76ae0787fe069c1ec83a2aceea
Markus Rossio <argitavis@gmail.com>
2022-02-12 01:43:59 -0600

    Add form skeleton to listing form component

--- src/Components/ListingForm/ListingForm.js
+++ src/Components/ListingForm/ListingForm.js
@@ -2,6 +2,23 @@ import React from "react";
 
 export default function ListingForm() {
   return (
-    <h1>Listing Form</h1>
+    <form>
+      <label>Photo URL</label>
+      <input type="text"/>
+      <label>Side</label>
+      <select>
+        <option value="left">Left</option>
+        <option value="right">Right</option>
+      </select>
+      <label>Size</label>
+      <input type="number"/>
+      <label>Style</label>
+      <select>
+        <option value="sneakers">Sneaker</option>
+        <option value="boots">Boot</option>
+        <option value="heels">Heel</option>
+        <option value="dress">Dress</option>
+      </select>
+    </form>
   )
 }
