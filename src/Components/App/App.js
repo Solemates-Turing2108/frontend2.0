cd57ee48d3891bac2f6787731a9adea205c83539
Tanya Zhuge <tangnazhuge@gmail.com>
2022-02-13 00:09:48 -0800

    Update import paths after component files moved

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import MainDisplay from './MainDisplay/MainDisplay';
-import DashboardPage from './DashboardPage/DashboardPage';
-import DetailsPage from './DetailsPage/DetailsPage';
+import MainDisplay from '../MainDisplay/MainDisplay';
+import DashboardPage from '../DashboardPage/DashboardPage';
+import DetailsPage from '../DetailsPage/DetailsPage';
 import ListingForm from '../ListingForm/ListingForm';
 
 function App() {
