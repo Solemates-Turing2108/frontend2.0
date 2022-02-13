91b0aa8890e6a2c6875a34c22b7940ed85ce632c
Tanya Zhuge <tangnazhuge@gmail.com>
2022-02-12 21:37:54 -0800

    Create new folder to reorganize files

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import MainDisplay from './MainDisplay/MainDisplay';
-import DashboardPage from './DashboardPage';
-import DetailsPage from './DetailsPage';
+import DashboardPage from './DashboardPage/DashboardPage';
+import DetailsPage from './DetailsPage/DetailsPage';
 import ListingForm from '../ListingForm/ListingForm';
 
 function App() {
