f61fd2c7f1ee53d5d6e77eeb2afa716390d7bd5d
Tanya Zhuge <tangnazhuge@gmail.com>
2022-02-14 14:29:28 -0800

    Update to display shoeListings in user's dashboard

--- src/Components/DashboardPage/DashboardPage.js
+++ src/Components/DashboardPage/DashboardPage.js
@@ -1,28 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import './DashboardPage.css';
 import { useLocation } from 'react-router-dom';
+import ShoeCard from '../ShoeCard/ShoeCard';
 
 
 const DashboardPage = () => {
 
     const [userData, setUserData ] = useState([]);
 
-    // const userID = useLocation().pathname
+    const userID = useLocation().pathname.split('/')[2]
     // console.log(userID)
+    // console.log('data', userData)
+
+    const shoesListings = userData.sort((shoe) => {
+      return shoe.date
+    }).map((each)=> { 
+      console.log(each)
+      return <ShoeCard key={each.id} shoeData={each}/>
+    });
 
     useEffect(() => {
         const fetchData = async (api) => {
           const response = await fetch(api)
           const responseJson = await response.json()
-          setUserData(responseJson)
+          setUserData(responseJson.shoes)
         } 
-        // fetchData(`https://turingsolemates.herokuapp.com/api/v1/users/{userID}/shoes`)
+        fetchData(`https://turingsolemates.herokuapp.com/api/v1/users/${userID}/shoes`)
       }, [])
 
       return (
     <>
-        <h1>SoleMate DashboardPage</h1>
-        {/* <p>{userData}</p> */}
+        <h1>Dashboard</h1>
+        <p>User's ID: {userID}</p>
+        <h3>All Shoes Listing Below</h3>'
+        <ul>
+          {shoesListings}
+        </ul>
     </>
   );
 }
