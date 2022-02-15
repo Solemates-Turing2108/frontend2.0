32a225115ae14a223a902d9f9b5d64fe21e2857d
Markus Rossio <argitavis@gmail.com>
2022-02-15 00:22:15 -0600

    Add user input handling to listing form submit
    Upon form submit all values are captured in an object with keys matching whats required by api post

--- src/Components/ListingForm/ListingForm.js
+++ src/Components/ListingForm/ListingForm.js
@@ -1,24 +1,50 @@
-import React from "react";
+import React, { useRef } from "react";
 
 export default function ListingForm() {
+  const photoURLRef = useRef();
+  const sideOptionRef = useRef();
+  const sizeInputRef = useRef();
+  const styleOptionRef = useRef();
+  const brandInputRef = useRef();
+  const descriptionInputRef = useRef();
+
+  const submitHandler =  (event) => {
+    event.preventDefault();
+    const userInputs = {
+      brand: brandInputRef.current.value,
+      description: descriptionInputRef.current.value,
+      photoURLRef: photoURLRef.current.value,
+      side: sideOptionRef.current.value,
+      size: sizeInputRef.current.value,
+      style: styleOptionRef.current.value,
+    };
+
+    return userInputs;
+  }
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
+      <label>Brand</label>
+      <input ref={brandInputRef} type="text" />
       <label>Photo URL</label>
-      <input type="text"/>
+      <input ref={photoURLRef} type="text"/>
+      <label>Description</label>
+      <input ref={descriptionInputRef}/>
       <label>Side</label>
-      <select>
+      <select ref={sideOptionRef}>
         <option value="left">Left</option>
         <option value="right">Right</option>
       </select>
       <label>Size</label>
-      <input type="number"/>
+      <input ref={sizeInputRef} type="number" />
       <label>Style</label>
-      <select>
+      <select ref={styleOptionRef}>
         <option value="sneakers">Sneaker</option>
         <option value="boots">Boot</option>
         <option value="heels">Heel</option>
         <option value="dress">Dress</option>
       </select>
+      <button type="submit">Submit</button>
     </form>
   )
 }
