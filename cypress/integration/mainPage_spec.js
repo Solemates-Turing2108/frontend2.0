138b586d5cb343ca7606ba56ca7760b878b297bf
Tanya Zhuge <tangnazhuge@gmail.com>
2022-02-16 03:52:01 -0800

    Add tests for navigation buttons

--- cypress/integration/mainPage_spec.js
+++ cypress/integration/mainPage_spec.js
@@ -25,6 +25,21 @@ describe('mainPage', () => {
             .contains('12')
      })
 
+    it('should have a Home button that links to the base url', () => {
+        cy.get('.nav').should('have.attr', 'href').and('include', '/')
+    .get('.home').should('have.attr', 'src').should('include', 'imageURL????')
+})
+
+
+    it('should have a Dashboard button that links to the user\'s personal listings', () => {
+        cy.get('.nav').should('have.attr', 'href').and('include', '/shoes')
+    .get('.home').should('have.attr', 'src').should('include', 'imageURL????')
+})
+
+    it('should have an add button that links to the form page to add a listing', () => {
+        cy.get('.nav').should('have.attr', 'href').and('include', '/shoes')
+    .get('.home').should('have.attr', 'src').should('include', 'imageURL????')
+})
 
 })
 
