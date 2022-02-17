87f5bdaaaca613c6a23e4852bd439e8802918808
Tanya Zhuge <tangnazhuge@gmail.com>
2022-02-16 23:12:09 -0800

    Update testing with correct html tags

--- cypress/integration/dashboardPage_spec.js
+++ cypress/integration/dashboardPage_spec.js
@@ -5,49 +5,54 @@ describe('Dashboard Page', () => {
     });
 
     it('should be able to visit the page and render the site\'s title', () => {
-        cy.get('h1')
-          .contains('Solemates')
-      });
+        cy.get('div[class="header_layout"]')
+            .should('exist')
+    });
 
-    it('should have a link inside of each card', () => {
-         cy.get('li')
-            .get('a')
-            .get('[href="/shoes/:5"]')
-    })
-
-    it('should have a details description and user id is 1', () => {
+    it('should have a link inside of each card to navigate to detailsPage of the shoe', () => {
         cy.get('li')
-            .get('p')
-            .contains('shoes')
+            .get('a')
+            .get('[href="/shoes/:5"]').click()
+            .get('li[class="product-card_container"]')
+            .should('not.exist')
+            .url().should('eq', 'http://localhost:3000/shoes/:5')
+    });
+
+    it('should have details on each shoe', () => {
+        cy.get('div[class="dashboard_layout"]')
+            .first()
             .next()
-            .contains('5')
-            .next()
-            .next()
-            .contains('right')
-            .next()
-            .contains('5')
-            .next()
-            .contains('sneaker')
-            .next()
-            .contains('1')
-            .next()
-            .get('button')
-            .contains('Delete')
-     })
+            .get('ul[class="dashboard_listings"]')
+                .children('li[class="product-card_container"]')
+                .next()
+                .get('p')
+                .contains('shoes')
+                .next()
+                .contains('nike')
+                .next()
+                .contains('5')
+                .next()
+                .contains('right')
+                .next()
+                .contains('sneaker')
+                .next()
+                .get('button')
+                .contains('Delete')
+     });
 
     it('should have a Home button that links to the base url', () => {
-        cy.get('.nav').should('have.attr', 'href').and('include', '/')
-    .get('.home').should('have.attr', 'src').should('include', 'imageURL????')
-})
+        cy.get('.navbar_button').children().should('have.attr', 'href').and('include', '/')
+            .get('img[alt="home"]').should('have.attr', 'src').should('include', '/static/media/noun-house-1711906.d012bb1964ee2948e465f46e7b44e02c.svg')
+    });
 
     it('should have a Dashboard button that links to the user\'s personal listings', () => {
-        cy.get('.nav').should('have.attr', 'href').and('include', '/shoes')
-    .get('.home').should('have.attr', 'src').should('include', 'imageURL????')
-})
+        cy.get('.navbar_button').children().eq(2).should('have.attr', 'href').and('include', '/dashboard')
+            .get('img[alt="home"]').eq(2).should('have.attr', 'src').should('include', '/static/media/noun-user-4602163.39134a6bc0eddc88594d21ffc73da21d.svg')
+    });
 
     it('should have an add button that links to the form page to add a listing', () => {
-        cy.get('.nav').should('have.attr', 'href').and('include', '/shoes')
-    .get('.home').should('have.attr', 'src').should('include', 'imageURL????')
-})
+        cy.get('.navbar_button').children().eq(1).should('have.attr', 'href').and('include', '/add-listing')
+        .get('img[alt="home"]').eq(1).should('have.attr', 'src').should('include', '/static/media/noun-plus-2048209.1423e678f69b25921d6684d548dd04d5.svg')
+    });
 
 }) 
